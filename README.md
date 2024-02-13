a606a6185949fbe2a7ce09ac553c70b29c54c8d0
Nathan Disidore <nathan@cloudflare.com>
2024-02-13 16:45:23 -0600

    Rework to bard-of-love

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-# Cheekfreaks Online
+# The Love Bard
 
-Dontcha hate it when AI doesn't know your favorite band? I'm obsessed with the band [Cheekface](https://linktr.ee/cheekface). I self identify as a Cheekfreak.
+Sometimes we could all use a little extra love, especially with Valentine's Day around the corner!
 
-This is an exploration using [Workers AI](https://developers.cloudflare.com/workers-ai/) w/ [Hono](https://hono.dev/getting-started/cloudflare-workers) and [LangChain](https://js.langchain.com/docs/integrations/vectorstores/cloudflare_vectorize) [abstractions](https://js.langchain.com/docs/integrations/chat/cloudflare_workersai) to build a super music fan that you can chat with.
+This is an exploration using [Workers AI](https://developers.cloudflare.com/workers-ai/) w/ [Hono](https://hono.dev/getting-started/cloudflare-workers) and [LangChain](https://js.langchain.com/docs/integrations/vectorstores/cloudflare_vectorize) [abstractions](https://js.langchain.com/docs/integrations/chat/cloudflare_workersai) to build a love poem reading bard that you can chat with.
 
-Lyrics are uploaded as text files to [R2](https://developers.cloudflare.com/r2/), which is then used to create [embeddings](https://js.langchain.com/docs/integrations/text_embedding/cloudflare_ai) and store them in Cloudflare's vector database [Vectorize](https://js.langchain.com/docs/integrations/text_embedding/cloudflare_ai).
-
-Chat pulls in relevant lyrics via a retriever.
+We seed poems from a static file in our app, (though conceivably you could also use [R2](https://developers.cloudflare.com/r2/)), which is then used to create [embeddings](https://js.langchain.com/docs/integrations/text_embedding/cloudflare_ai) and store them in Cloudflare's vector database [Vectorize](https://js.langchain.com/docs/integrations/text_embedding/cloudflare_ai).
 
 This, like all of us, is a work in progress.
 
@@ -20,16 +18,14 @@ Set up your secrets
 
 ```bash
 npx wrangler secret put CLOUDFLARE_ACCOUNT_ID
-npx wrangler secret put CLOUDFLARE_CLOUDFLARE_API_TOKEN
+npx wrangler secret put CLOUDFLARE_API_TOKEN
 ```
 
 Create your Vector database
 
 ```bash
-npx wrangler vectorize create lyrics --preset "@cf/baai/bge-base-en-v1.5"
+npx wrangler vectorize create ndisidore-poems --preset "@cf/baai/bge-base-en-v1.5"
 ```
-
-Upload songs in separate files to R2
 
 ## Deploy
 
--- src/index.js
+++ src/index.js
@@ -5,40 +5,15 @@ import { HumanMessagePromptTemplate, SystemMessagePromptTemplate, ChatPromptTemp
 import { RunnableSequence, RunnablePassthrough } from '@langchain/core/runnables';
 import { StringOutputParser } from '@langchain/core/output_parsers';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { formatDocumentsAsString } from "langchain/util/document";
 import { ChatCloudflareWorkersAI, CloudflareVectorizeStore, CloudflareWorkersAIEmbeddings } from '@langchain/cloudflare';
+import rawPoemNdJson from '../assets/poems.ndjson';
 
 const app = new Hono();
 
 const EMBEDDINGS_MODEL = '@cf/baai/bge-base-en-v1.5';
 
-async function getSongs(bucket) {
-	const listed = await bucket.list();
-	const keys = listed.objects.map((obj) => obj.key);
-	const songs = [];
-	for (const key of keys) {
-		const object = await bucket.get(key);
-		const lyrics = await object.text();
-		const title = lyrics.split('\n')[0];
-		songs.push({
-			title,
-			lyrics,
-			key,
-		});
-	}
-	return songs;
-}
-
 app.get('/', serveStatic({ root: './index.html' }));
-
-function formatDocsToLyrics(docs) {
-	return docs
-		.map((doc) => {
-			return `Song: ${doc.metadata.title}
-    
-    ${doc.pageContent}`;
-		})
-		.join('\n\n');
-}
 
 app.post('/prompt', async (c) => {
 	const body = await c.req.json();
@@ -49,6 +24,7 @@ app.post('/prompt', async (c) => {
 	const store = new CloudflareVectorizeStore(embeddings, {
 		index: c.env.VECTORIZE_INDEX,
 	});
+
 	console.log(`Setting AI model ${body.model}`);
 	const chat = new ChatCloudflareWorkersAI({
 		model: body.model,
@@ -65,29 +41,25 @@ app.post('/prompt', async (c) => {
 	console.log('Trying to send the user message', userMessage);
 	const prompt = ChatPromptTemplate.fromMessages([
 		SystemMessagePromptTemplate.fromTemplate(`
-    You are a huge fan of the band Cheekface. 
-    
-    You are known as a Cheekfreak. 
-    
-    You are obsessed with the band and especially their lyrics, you always quote them.
+    Today is Valentine's Day and you are spreading love to all those you chat with.
 
-    The lyrics are included below and are relevant to your conversation. 
-    
-    Use at least one lyric, and/or song title in context of the conversation in every single response. 
-    
-    Stay conversational and quirky to keep things moving.
-    
-    You should never talk about their albums, only about the lyrics.
+    You are known as a Bernard the Bard of Love.
 
-    Limit yourself to 5 or fewer sentences.
-    
-    Do not make up your own lyrics.
+	You want to shower users with love, especially love poems, and are always quoting them.
 
-    If you cannot find a relevant one for the conversation, just say the first lyric.
-    
-    <lyrics>
-    {lyrics}
-    </lyrics>
+    The poems are included below and are relevant to your conversation.
+
+    Use at least two lines from a poem in every single response of yours.
+
+    Do not make up your own poems.
+
+    If you cannot find a relevant poem for the conversation, just say the first stanza.
+
+	Limit responses to 5 or fewer sentences.
+
+    <poem>
+    {poems}
+    </poem>
     `),
 		...messageObjects,
 		HumanMessagePromptTemplate.fromTemplate('{userMessage}'),
@@ -95,7 +67,7 @@ app.post('/prompt', async (c) => {
 
 	const chain = RunnableSequence.from([
 		{
-			lyrics: vectorStoreRetriever.pipe(formatDocsToLyrics),
+			poems: vectorStoreRetriever.pipe(formatDocumentsAsString),
 			userMessage: new RunnablePassthrough(),
 		},
 		prompt,
@@ -133,21 +105,30 @@ app.get('/load-em-up', async (c) => {
 	const store = new CloudflareVectorizeStore(embeddings, {
 		index: c.env.VECTORIZE_INDEX,
 	});
-	console.log('Getting songs from R2');
-	const songs = await getSongs(c.env.LYRICS_BUCKET);
-	console.log(`Retrieved ${songs.length}`);
+
+	console.log('Getting poems from our static file');
+	/** @type {{title: string; author: string; text: string}[]} */
+	const poems = rawPoemNdJson.split('\n').map((line) => {
+		/** @type {{title: string; author: string; text: string}} */
+		const parsed = JSON.parse(line);
+		parsed.text = parsed.text.replaceAll('|', '\n');
+		console.log(parsed.text);
+		return parsed;
+	});
+	console.log(`Retrieved ${poems.length}`);
+
 	// Chunk and split
 	const splitter = new RecursiveCharacterTextSplitter({ chunkSize: 200, chunkOverlap: 10 });
-	for (const song of songs) {
-		const docs = await splitter.createDocuments([song.lyrics], [{ title: song.title, key: song.key }]);
-		console.log(`${song.title} created ${docs.length} docs`);
+	for (const poem of poems) {
+		const docs = await splitter.createDocuments([poem.text], [{ title: poem.title, author: poem.author }]);
+		console.log(`${poem.title} created ${docs.length} docs`);
 		console.log('First one:', JSON.stringify(docs[0]));
 		console.log(`Adding to Vectorize`);
-		// FIXME: Vectorize doesn't like this loc `object` type
-		//docs.forEach((doc) => delete doc.metadata.loc);
+		docs.forEach((doc) => delete doc.metadata.loc); // Vectorize metadata has restrictions on its shape
 		const indexedIds = await store.addDocuments(docs);
 		console.log(`Inserted ${JSON.stringify(indexedIds)}`);
 	}
+	return new Response(null, { status: 200 });
 });
 
 export default app;
